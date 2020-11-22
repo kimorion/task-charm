1f5547abdac619e63b56d32187831a4129e8ab10
Kirill <kimorion@ya.ru>
2020-11-22 16:37:54 +0300

    add migrations

---
+++ Charm.Core/Migrations/20201122133632_Init.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Charm.Core.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gists",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParentGistId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Gists_Gists_ParentGistId",
+                        column: x => x.ParentGistId,
+                        principalTable: "Gists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Gists_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reminders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    GistId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Deadline = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Advance = table.Column<TimeSpan>(type: "interval", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reminders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reminders_Gists_GistId",
+                        column: x => x.GistId,
+                        principalTable: "Gists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gists_ParentGistId",
+                table: "Gists",
+                column: "ParentGistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gists_UserId",
+                table: "Gists",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reminders_Deadline",
+                table: "Reminders",
+                column: "Deadline");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reminders_GistId",
+                table: "Reminders",
+                column: "GistId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reminders");
+
+            migrationBuilder.DropTable(
+                name: "Gists");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
